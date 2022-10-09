268d4dc5aa12a816c288ab7c57e9ca025ab8ddfe
Eliane V <eliane.vdc@protonmail.com>
2022-10-09 19:18:22 +0100

    menubar div correct height vh

--- components/menubar.js
+++ components/menubar.js
@@ -1,11 +1,13 @@
 export default function Menubar() {
   return (
     <>
-      <div className="md:w-60 justify-around bg-black">
-        <h1 className=" p-3">MENU 1</h1>
-        <h1 className=" p-3">MENU 2</h1>
-        <h1 className=" p-3">MENU 3</h1>
-        <h1 className=" p-3">MENU 4</h1>
+      <div className="h-[calc(100%-3rem)] flex items-center">
+        <div className="md:w-60 h-5/6 mx-10 justify-around bg-slate-50 rounded-md">
+          <h1 className=" p-3">MENU 1</h1>
+          <h1 className=" p-3">MENU 2</h1>
+          <h1 className=" p-3">MENU 3</h1>
+          <h1 className=" p-3">MENU 4</h1>
+        </div>
       </div>
     </>
   );
--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <>
-      <div className="bg-slate-700 flex justify-between">
+      <div className="bg-slate-700 h-12 flex justify-between">
         <h1 className="text-white p-3">LOGO</h1>
         <h1 className="text-white p-3">LOGOUT</h1>
       </div>
