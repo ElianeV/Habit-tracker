4815d9cc4f24e1a000f329e361ee7d676f01be32
Eliane V <eliane.vdc@protonmail.com>
2022-10-12 10:08:15 +0100

    add routes to pages

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,17 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <>
       <div className="h-12 flex justify-between border-b bg-transparent">
         <h1 className="p-3">LOGO</h1>
         <div className="flex flex-end">
-          <h1 className="p-3">Calendar</h1>
-          <h1 className="p-3">Manage habits</h1>
+          <Link href="/">
+            <a className="p-3">Calendar</a>
+          </Link>
+          <Link href="/habitmanager">
+            <a className="p-3">Manage habits</a>
+          </Link>
           <h1 className="p-3">Logout</h1>
         </div>
       </div>
