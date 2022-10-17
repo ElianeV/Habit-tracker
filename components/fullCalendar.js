7075a02acf8daabc03a5ff523e15d5ee0ec0a265
Eliane V <eliane.vdc@protonmail.com>
2022-10-17 16:16:56 +0100

    finish banner habitcontainer

--- components/fullCalendar.js
+++ components/fullCalendar.js
@@ -16,7 +16,7 @@ export default function FullCalendar({ habits }) {
   return (
     <>
       <div className="w-2/3 h-5/6 mr-10 border-neutral-200 border-solid border-2 border-l-0 rounded-3xl rounded-l-none flex flex-col overflow-x-scroll">
-        <div className="h-16 w-400 flex justify-between p-1 border-b">
+        <div className="h-16 w-600 flex justify-between p-1 border-b">
           {dates.map((date) => (
             <div className="w-10" key={date.getDate()}>
               <div>{date.toLocaleString(undefined, options)}</div>
--- components/habitcontainer.js
+++ components/habitcontainer.js
@@ -1,12 +1,14 @@
 export default function Habitcontainer({ habits }) {
   return (
     <>
-      <div className="w-1/3 h-5/6 ml-10 border-neutral-200 border-solid border-2 border-r-0 rounded-3xl rounded-r-none shadow-lg bg-slate-50 flex flex-col">
-        <div className="h-16 p-1 border-b flex justify-between">
-          <div>My habits</div>
+      <div className="w-1/3 h-5/6 ml-10 border-neutral-200 border-solid border-2 border-r-0 rounded-3xl rounded-r-none shadow-lg bg-slate-50 flex flex-col overflow-hidden">
+        <div className="h-20 pl-10 pr-4 border-b flex justify-between items-center bg-gradient-to-r from-violet-500 to-violet-50">
+          <div className="text-2xl">My habits</div>
           <div>
-            <p>{new Date().toLocaleString("default", { month: "long" })}</p>
-            <p className="text-right">{new Date().getFullYear()}</p>
+            <p className="text-right text-xl">
+              {new Date().toLocaleString("default", { month: "long" })}
+            </p>
+            <p className="text-right font-bold">{new Date().getFullYear()}</p>
           </div>
         </div>
         {habits.map((habit) => (
